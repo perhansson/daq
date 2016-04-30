d0492106918dcd5e7b9cf2eff8f8f5a66a5fe3bc
Per Hansson Adrian <per.hansson80@gmail.com>
2016-04-29 17:26:49 -0700

    Add basic skeleton for cPix frame

--- pix-dev/python/epix/frame.py
+++ pix-dev/python/epix/frame.py
@@ -10,6 +10,87 @@ from epix import Epix100a, fmap
 from pix_utils import FrameTimer as timer
 
 
+class CpixFrame(object):
+    '''Convert readout order into an ordered pixel image for Epix100a.'''
+    nx = 768
+    ny = 708
+    n_head=8
+    n_super_rows = 710
+    n_env_super_rows = 2
+    n_tps_words = 2
+    n_footer_words = 1
+    len_super_row = nx
+    #nblocks=16;
+    #nbcols=92;
+    npix=nx*ny;
+    framesize= n_head + n_super_rows*len_super_row/2 + n_tps_words + n_footer_words
+    n_asics = 4
+
+    def __init__(self,data = None, asic=-1):
+
+        # store a full readout frame
+        self.super_rows = np.zeros([EpixFrame.ny, EpixFrame.nx], dtype=np.int16)
+
+        # set the data if available
+        if data != None:
+            self.set_data_fast(data)        
+            #self.set_data(data,asic)        
+
+        # clusters
+        self.clusters = []
+
+
+    def set_data_fast(self,data):
+        """ Organize into raw super rows """
+        #print('set super rows')
+
+        t0 = timer('set_data_fast')
+        t0.start()
+        
+        nx = EpixFrame.nx
+        ny = EpixFrame.ny
+        i = 0
+
+        # find the pixel data only
+        offset_start = EpixFrame.n_head 
+        #offset_start = i*EpixFrame.nx/2 +  EpixFrame.n_head 
+        #offset_end = (i+1)*EpixFrame.nx/2 + EpixFrame.n_head
+        #frame_data2 = data[offset_start:offset_end]        
+        #frame_data2 = data[offset_start:offset_end]        
+        # convert to array!
+        frame_data = np.asarray(data[offset_start:((nx/2)*ny+offset_start)],dtype=np.uint32)
+
+        # read out the packed pixel adc values
+        frame_data_odd = (frame_data >> 16) & 0xFFFF 
+        frame_data_even  = frame_data & 0xFFFF 
+        #frame_data_even = (frame_data >> 16) & 0xFFFF 
+        #frame_data_odd  = frame_data & 0xFFFF 
+
+        # new int16 array to hold final result
+        frame_data_new = np.empty(ny*nx, dtype=np.int16)
+
+        #print('frame_data ' + str(np.shape(frame_data)))
+        #print('frame_data_even ' + str(np.shape(frame_data_even)))
+        #print('frame_data_odd ' + str(np.shape(frame_data_odd)))
+        #print('frame_data_new ' + str(np.shape(frame_data_new)))
+        #print('frame_data_new[::2] ' + str(np.shape(frame_data_new[::2])))
+        #print('frame_data_new[1::2] ' + str(np.shape(frame_data_new[1::2])))
+
+        # set even and odd columns
+        frame_data_new[::2] = frame_data_even.astype(np.int16)
+        frame_data_new[1::2] = frame_data_odd.astype(np.int16)
+        
+        # do some magic to reshape the super rows
+        out = np.reshape(frame_data_new, (ny//2, nx*2))
+        out = np.concatenate( (np.flipud( out[:,nx:2*nx] ), out[:,0:nx]), axis=0)
+        
+        self.super_rows = out
+
+        t0.stop()
+        #print('[EpixFrame]: ' + t0.toString())
+
+
+
 class EpixFrame(object):
     '''Convert readout order into an ordered pixel image for Epix100a.'''
     nx = 768
